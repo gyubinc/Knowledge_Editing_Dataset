6b52cd4836eb5f6fcac15c2041fa0420cfb41818
gyubinc <yeppi315@gmail.com>
2024-03-18 19:38:56 +0900

    0318 회의

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import requests
 import yaml
+import json
 
 def load_json_from_url(url):
     response = requests.get(url)
@@ -9,7 +10,15 @@ def load_json_from_url(url):
     else:
         print("실패") 
         return None
-    
+
+def load_jsonl_from_path(file_path):
+    data = []
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            json_obj = json.loads(line.strip())
+            data.append(json_obj)
+    return data
+
 def load_config(config_file):
     with open(config_file) as file:
         config = yaml.safe_load(file)
